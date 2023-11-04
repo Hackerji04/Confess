f82670e12aa323c4b4a342da11837bd042c12abe
Hackerji04 <113308263+Hackerji04@users.noreply.github.com>
2023-11-04 16:40:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+
+let hiddenParagraph = document.getElementById("hiddenParagraph");
+let handL = document.querySelector(".hand-l");
+let handR = document.querySelector(".hand-r");
+
+// // let normalEyeStyle = () => {
+// //   eyeL.style.cssText = "left: 0.6em; top: 0.6em;";
+// //   eyeR.style.cssText = "right: 0.6em; top: 0.6em;";
+// // };
+
+// let normalHandStyle = () => {
+  
+//   handL.style.cssText =
+//   "height: 6.56em; top: 3.87em; left: 11.75em; transform: rotate(-155deg);animation: slide 150s linear;";
+//   handR.style.cssText =
+//   "height: 6.56em; top: 3.87em; right: 11.75em; transform: rotate(155deg);animation: slide 150s linear;";
+//   window.location.href = "panda.html";;
+// };
+
+// // When clicked on username input
+// // usernameRef.addEventListener("focus", () => {
+// //   eyeL.style.cssText = "left: 0.75em; top: 1.12em;";
+// //   eyeR.style.cssText = "right: 0.75em; top: 1.12em;";
+// //   normalHandStyle();
+// // });
+
+// // When clicked on password input
+// // passwordRef.addEventListener("focus", () => {
+// //   handL.style.cssText =
+// //     "height: 6.56em; top: 3.87em; left: 11.75em; transform: rotate(-155deg);";
+// //   handR.style.cssText =
+// //     "height: 6.56em; top: 3.87em; right: 11.75em; transform: rotate(155deg);";
+// //   // normalEyeStyle();
+// // });
+
+// function panda() {
+//   // handL.style.cssText =
+//   //   "height: 6.56em; top: 3.87em; left: 11.75em; transform: rotate(-155deg);";
+//   // handR.style.cssText =
+//   //   "height: 6.56em; top: 3.87em; right: 11.75em; transform: rotate(155deg);";
+// //   normalHandStyle();
+// onclick="window.location.href = 'https://ide.geeksforgeeks.org';"
+// }
+// let passwordRef = document.getElementById("pass");
+// passwordRef.addEventListener("click", {
+//     window.location.href = "/assets/confess.html";
+// }
+    
+
+// );
+
+
+// When clicked outside username and password input
+// document.addEventListener("click", (e) => {
+//   let clickedElem = e.target;
+//   if (clickedElem = passwordRef) {
+//     // normalEyeStyle();
+//     normalHandStyle();
+//   }
+// });
